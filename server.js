e89402bade2902cd1be81758ef25ea338dfeabba
AbShamilDev <abkarov02@gmail.com>
2024-06-14 03:50:18 +0300

    hello world

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.get("/", (req, res) => {
+  res.send("WebSocket сервер работает!");
+});
+
+// Обработчик WebSocket соединений
+wss.on("connection", (ws) => {
+  console.log("Новое соединение установлено");
+
+  ws.on("message", (message) => {
+    console.log("Получено сообщение: %s", message);
+    // Отправка сообщения обратно клиенту
+    ws.send(`Эхо: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Соединение закрыто");
+  });
+});
+
+// Запуск сервера
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
